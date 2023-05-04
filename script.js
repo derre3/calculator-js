ddfd4d69b09ec8a45589aa8413370bf7d985a0fe
Andre <123985988+derre3@users.noreply.github.com>
2023-05-04 13:56:22 -0300

    add basic operation functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num, op) {
+    return op(num[0], num[1]);
+}
