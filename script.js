d5c3c6089bed610b60637cf2e7ce39af7726026b
Andre <123985988+derre3@users.noreply.github.com>
2023-05-05 14:07:06 -0300

    add basic calc functionality (populateDisplay and clearCalc working)

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ function divide(a, b) {
 function operate(num, op) {
     return op(num[0], num[1]);
 }
+
+function populateDisplay() {
+    return textDisplay.textContent = buttonClicked.join('');
+}
+
+function clearCalc() {
+    buttonClicked = [];
+    return textDisplay.textContent = '';
+}
+
+const textDisplay = document.querySelector('.calculator-display');
+textDisplay.textContent = ''
+let buttonClicked = []
+
+// click event for number buttons
+const buttonNum = document.querySelectorAll('.button.num');
+for (let i = 0; i < buttonNum.length; i++) {
+    buttonNum[i].addEventListener("click", () => {
+        buttonClicked.push(buttonNum[i].textContent);
+        populateDisplay();
+    });
+}
+
+// click event for AC button
+const buttonClear = document.querySelector('.button.clear');
+buttonClear.addEventListener('click', clearCalc);
